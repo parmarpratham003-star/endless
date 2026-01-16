31552b98f5d8a58597ee3550b562e23f5268946a
parmarpratham003-star <parmarpratham003@gmail.com>
2026-01-16 10:38:48 +0530

    5

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       animation: {
+//         gradient: "gradientMove 18s ease infinite",
+//         floatSlow: "floatSlow 10s ease-in-out infinite",
+//         floatSlower: "floatSlower 14s ease-in-out infinite",
+//       },
+//       keyframes: {
+//         gradientMove: {
+//           "0%, 100%": { backgroundPosition: "0% 50%" },
+//           "50%": { backgroundPosition: "100% 50%" },
+//         },
+//         floatSlow: {
+//           "0%, 100%": { transform: "translateY(0px)" },
+//           "50%": { transform: "translateY(-30px)" },
+//         },
+//         floatSlower: {
+//           "0%, 100%": { transform: "translateY(0px)" },
+//           "50%": { transform: "translateY(40px)" },
+//         },
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
